1c6902f1db8f1f93476dc9f4d2edc7e115fa8838
Luciiver <farhan42003@gmail.com>
2024-03-26 03:40:33 +0700

    add

---
+++ Assets/Scripts/BumperController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperController : MonoBehaviour
+{
+    public Collider bola;
+    public float multiplier;
+    public Color color;
+
+    public AudioManager audioManager;
+    public VFXManager vfxManager;
+
+    private Renderer renderer;
+    private Animator animator;
+
+   private void Start()
+   {
+
+        renderer = GetComponent<Renderer>();
+        animator = GetComponent<Animator>();
+
+        renderer.material.color = color;
+
+   } 
+   private void OnCollisionEnter(Collision collision)
+   {
+        if (collision.collider == bola)
+        {
+            Rigidbody bolaRig = bola.GetComponent<Rigidbody>();
+            bolaRig.velocity *= multiplier;
+          
+          //animasi
+            animator.SetTrigger("hit");
+
+           //Playsfx
+            audioManager.PlaySFX(collision.transform.position);
+
+            //playvfx
+            vfxManager.PlayVFX(collision.transform.position);
+          
+        }
+   }
+}
